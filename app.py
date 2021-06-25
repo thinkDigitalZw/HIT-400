ff527404f8febb4e60dfc5ac98cdfc2571a71574
thinkDigitalZw <thinkdigitalzw@gmail.com>
2021-06-25 05:37:15 +0200

    Connected model with frontend and served web app through flask

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from joblib import dump, load
 
 
 with open(f'models/randomForest.joblib', 'rb') as f:
-    randomForestModel = load(f)
+    randomForest = load(f)
 
 
 app = flask.Flask(__name__, template_folder='templates')
@@ -17,29 +17,30 @@ def main():
     if flask.request.method == 'GET':
         return (flask.render_template('index.html'))
 
-    # if flask.request.method == 'POST':
-    #     rooms = flask.request.form['rooms']
-    #     bathroom = flask.request.form['bathroom']
-    #     landsize = flask.request.form['landsize']
-    #     lattitude = flask.request.form['lattitude']
-    #     longtitude = flask.request.form['longtitude']
-    #     distance = flask.request.form['distance']
-    #     car = flask.request.form['car']
-    #     landsize = flask.request.form['landsize']
-    #     buildingarea = flask.request.form['buildingarea']
-    #     yearbuilt = flask.request.form['yearbuilt']
+    if flask.request.method == 'POST':
+        age = flask.request.form['age']
+        anaemia = flask.request.form['anaemia']
+        creatine_phosphokinase = flask.request.form['creatine_phosphokinase']
+        diabetes = flask.request.form['diabetes']
+        ejection_fraction = flask.request.form['ejection_fraction']
+        high_blood_pressure = flask.request.form['high_blood_pressure']
+        platelets = flask.request.form['platelets']
+        serum_creatinine = flask.request.form['serum_creatinine']
+        serum_sodium = flask.request.form['serum_sodium']
+        sex = flask.request.form['sex']
+        smoking = flask.request.form['smoking']
+        time = flask.request.form['time']
 
-    #     input_variables = pd.DataFrame([[rooms, bathroom, landsize, lattitude, longtitude, distance, car, landsize, buildingarea, yearbuilt]],
-    #                                    columns=['rooms', 'bathroom', 'landsize', 'lattitude', 'longtitude',
-    #                                             'distance', 'car', 'landsize', 'buildingarea', 'yearbuilt'],
-    #                                    dtype='float',
-    #                                    index=['input'])
+        input_variables = pd.DataFrame([[age,anaemia, creatine_phosphokinase, diabetes, ejection_fraction, high_blood_pressure, platelets, serum_creatinine, serum_sodium, sex, smoking, time]],
+        columns=['age', 'anaemia', 'creatine_phosphokinase', 'diabetes', 'ejection_fraction', 'high_blood_pressure', 'platelets', 'serum_creatinine', 'serum_sodium', 'sex', 'smoking', 'time' ],
+        dtype='float',
+        index=['input'])
 
-    #     predictions = model.predict(input_variables)[0]
-    #     print(predictions)
+        predictions = randomForest.predict(input_variables)[0]
+        print(predictions,'WTF')
 
-    #     return flask.render_template('main.html', original_input={'Rooms': rooms, 'Bathroom': bathroom, 'Landsize': landsize, 'Lattitude': lattitude, 'Longtitude': longtitude, 'Distance': distance, 'Car': car, 'Landsize': landsize, 'BuildingArea': buildingarea, 'YearBuilt': yearbuilt},
-    #                                  result=predictions)
+        return flask.render_template('index.html', original_input={'Age': age, 'Anaemia': anaemia, 'Creatine Phosphokinase': creatine_phosphokinase, 'Diabetes': diabetes, 'Ejection Fraction': ejection_fraction, 'High Blood Pressure': high_blood_pressure, 'Platelets': platelets, 'Serum Creatinine': serum_creatinine, 'Serum Sodium': serum_sodium, 'Sex': sex, 'Smoking': smoking, 'Time':time  },
+                                     result=predictions)
 
 
 if __name__ == '__main__':
